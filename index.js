ef5ad626013cf8adbc82041ea3aa537acea1bd84
Tekalig <tekahazi06@gmail.com>
2024-04-09 09:39:38 +0300

    modified the style.css and index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let userScore = document.getElementById('user-score');
+let computerScore = document.getElementById('computer-score');
+let result = document.getElementsById('result');
+let rock = document.getElementById('r');
+let paper = document.getElementById('p');
+let scissors = document.getElementById('s');
+// console.log(userScore);
+console.log(result.textContent)
 function getComputerChoice() {
     // Array of choices
     const choices = ['rock', 'paper', 'scissors'];
@@ -9,4 +17,31 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
-console.log(getComputerChoice()); // Output: rock
+// console.log(getComputerChoice()); // Output: rock
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        result.textContent = "It's a tie!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        // won++;
+        result.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        // lost++;
+        result.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function playGame() {
+    // Generate a random index
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Enter your choice: rock, paper or scissors').toLowerCase();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+    // console.log(`You won ${won} times and lost ${lost} times`);
+}
+
+// playGame();
